9f374f9d895250e340c286da0c5066e470209d42
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2022-04-09 00:37:31 +0530

    First Commit

--- components/About.vue
+++ components/About.vue
@@ -1,8 +1,66 @@
 <template>
   <div id="About">
+    <div id="content-mobile">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/3939/3939627.png"
+        v-if="!bulbOn"
+        height="200px"
+        width="200px"
+        class="md:mx-auto ml-28 pt-12"
+      />
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/3939/3939470.png"
+        v-else
+        height="200px"
+        width="200px"
+        class="md:mx-auto ml-28 pt-12"
+      />
+
+      <button
+        type="button"
+        @click="turnOn()"
+        class="
+          mx-20
+          mt-8
+          text-white
+          bg-yellow-400
+          hover:bg-yellow-500
+          focus:outline-none focus:ring-4 focus:ring-yellow-300
+          font-medium
+          rounded-full
+          text-sm
+          px-5
+          py-2.5
+          text-center
+          mr-2
+          mb-2
+        "
+      >
+        Turn Bulb ON
+      </button>
+      <button
+        type="button"
+        @click="turnOff()"
+        class="
+          text-white
+          bg-purple-700
+          hover:bg-purple-800
+          focus:outline-none focus:ring-4 focus:ring-purple-300
+          font-medium
+          rounded-full
+          text-sm
+          px-5
+          py-2.5
+          text-center
+          mb-2
+        "
+      >
+        Turn Bulb OFF
+      </button>
+    </div>
     <div class="grid grid-cols-3 gap-4">
       <div class="pt-1 md:pt-28">
-        <Bulb id="content-desktop"/>
+        <Bulb id="content-desktop" />
       </div>
       <div class="col-span-3 md:col-span-2 container mx-auto md:px-32 px-8">
         <h1
